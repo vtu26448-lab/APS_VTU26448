3dfb46ba8c17af0fe3b7845554ecde92f7858243
tejababu <vtu26448@veltech.edu.in>
2026-02-20 10:01:09 +0530

    Time: 8 ms (1.94%) | Memory: 47.8 MB (54.21%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -7,18 +7,44 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+
+// // normal binary tree lca code also works but not considered as that optimised ..... tc : O(N)
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//          if (root == null || root == p || root == q) {
+//             return root;
+//         }
+
+//         TreeNode leftLca = lowestCommonAncestor(root.left, p, q);
+//         TreeNode rightLca = lowestCommonAncestor(root.right, p, q);
+
+//         if (leftLca == null) {
+//             return rightLca;
+//         }
+
+//         if (rightLca == null) {
+//             return leftLca;
+//         }
+
+//         return root;
+//     }
+// }
+
+// // bst lca preffered approach ..... tc : O(H) → balanced BST me O(log N)
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return root;
-        
-        if ((p.val <= root.val && q.val >= root.val) ||
-            (p.val >= root.val && q.val <= root.val)) {
-            return root;
+        while(root != null){
+            if(p.val < root.val && q.val < root.val){
+                root = root.left;
+            }    
+            else if(p.val > root.val && q.val > root.val){
+                root = root.right;
+            }
+            else{
+                return root;
+            }
         }
-
-        if (p.val > root.val) {
-            return lowestCommonAncestor(root.right, p, q);
-        }
-        return lowestCommonAncestor(root.left, p, q);
+        return null;
     }
 }
